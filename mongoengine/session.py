34ef18d29417cc0eec20896bc4e6859426fca72f
Fengyang Gao <FYGao@wish.com>
2019-04-19 16:45:46 +0800

    fix session support

--- mongoengine/session.py
+++ mongoengine/session.py
@@ -13,34 +13,24 @@ __all__ = ['start_session', 'start_transaction']
 
 class ClientSessionContext():
     def __init__(self, session):
-        # self._session stores the original session returned from client (ClientSession or MotorClientSession,
-        # depending on whether the client is async)
-        # self.session stores pymongo.client_session.ClientSession
-        self._session = session
-        if session.__class__.__name__.endswith('MotorClientSession'):
-            # unwrap MotorClientSesssion
-            self.session = session.delegate
-        else:
-            self.session = session
+        self.session = session
 
     def __enter__(self):
-        # always return/expose pymongo ClientSession
         return self.session
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         # use the original session
-        wait_for_future(self._session.end_session())
+        wait_for_future(self.session.end_session())
 
 
 def start_session(conn_name=None, causal_consistency=True,
-            default_transaction_options=None, allow_async=True):
+            default_transaction_options=None):
     """
-        It's not recommanded to use allow_async parameter.
         sample usage:
         with start_session(conn_name) as session:
             User.update({"id":"1"}, {"$set": {"username": "1"}}, session=session)
     """
-    allow_async &= bool(greenlet.getcurrent().parent)
+    allow_async = bool(greenlet.getcurrent().parent)
     conn = _connections[conn_name]
     client = conn.async if allow_async else conn.sync
     session = wait_for_future(client.start_session(
@@ -69,9 +59,8 @@ class TransactionContext():
 
 
 def start_transaction(session, read_concern=None, write_concern=None,
-            read_preference=None, allow_async=True):
+            read_preference=None):
     """
-        It's not recommanded to use allow_async parameter.
         Use find_and_modify inside transaction to avoid stale reads. https://docs.mongodb.com/manual/core/transactions-production-consideration/
         sample usage:
         with start_session(db_name) as session:
@@ -79,12 +68,8 @@ def start_transaction(session, read_concern=None, write_concern=None,
                 User.update({"id":"1"}, {"$set": {"username": "1"}}, session=session)
 
     """
-    allow_async &= bool(greenlet.getcurrent().parent)
-    if allow_async:
-        if not session.__class__.__name__.endswith('MotorClientSession'):
-            # need to wrap pymongo ClientSession
-            session = MotorClientSession(session)
-    else:
+    allow_async = bool(greenlet.getcurrent().parent)
+    if not allow_async:
         if session.__class__.__name__.endswith('MotorClientSession'):
             # need to unwrap
             session = session.delegate
